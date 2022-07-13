b5bd8c36434f3f5a0fedeb79d05d39aff8eaa680
ckcomin <com20009@byui.edu>
2022-07-13 10:47:05 -0600

    enemies collison file

---
+++ Dino/script/HandleEnemiesCollison.cs
@@ -0,0 +1,12 @@
+using genie;
+using genie.cast;
+using genie.script;
+using genie.services;
+using genie.services.raylib;
+
+namespace enemy.script
+{
+    class HandleEnemiesCollison {
+        
+    }
+}
